44d7b82b29ff7740937bfaba159603cca3f07875
kris52h6 <kris52h6@stud.kea.dk>
2022-11-28 13:49:31 +0100

    all matches styling changes

--- pages/allMatches/allMatches.js
+++ pages/allMatches/allMatches.js
@@ -32,24 +32,48 @@ function createKeyValuePairs(teams) {
 }
 
 function displayMatches(matchesData) {
-    console.log(matchesData);
-    let tableData = matchesData.map(
-        (m) =>
-            `
-        <tr id = match-id${m.id}>
-            <td>${teamsKeyValue.get(m.homeTeamId)}</td>
-            <td>${teamsKeyValue.get(m.awayTeamId)}</td>
-            <td>"${m.divisionName}"</td>
-            <td>${m.startTime}</td>
-        </tr>
-        `
-    );
-    const tableString = tableData.join("\n");
-    document.querySelector("#tbody").innerHTML = sanitizeStringWithTableRows(tableString);
+    document.querySelector("#match-content").innerHTML = "";
+    matchesData.forEach((m) => {
+        displayMatch(m);
+    });
 }
 
+function displayMatch(m) {
+    const matchTemplate = document.querySelector("template");
+    const clone = matchTemplate.cloneNode(true).content;
+    const matchContent = document.querySelector("#match-content");
+    const match = document.createElement("div");
+    match.classList.add("match");
+
+    clone.querySelector(".match-time").textContent = m.startTime;
+    clone.querySelector(".hometeam-h2").textContent = teamsKeyValue.get(m.homeTeamId);
+    // clone.querySelector(".hometeam-img").src = "./images/logos/oakslogo.png";
+    clone.querySelector(".awayteam-h2").textContent = teamsKeyValue.get(m.awayTeamId);
+    // clone.querySelector(".awayteam-img").src = "./images/logos/oakslogo.png";
+    match.id = "match-id" + m.id;
+
+    match.appendChild(clone);
+    matchContent.append(match);
+}
+
+// function displayMatches(matchesData) {
+//     console.log(matchesData);
+//     let tableData = matchesData.map(
+//         (m) =>
+//             `
+//         <tr id = match-id${m.id}>
+//             <td>${teamsKeyValue.get(m.homeTeamId)}</td>
+//             <td>${teamsKeyValue.get(m.awayTeamId)}</td>
+//             <td>"${m.divisionName}"</td>
+//             <td>${m.startTime}</td>
+//         </tr>
+//         `
+//     );
+//     const tableString = tableData.join("\n");
+//     document.querySelector("#tbody").innerHTML = sanitizeStringWithTableRows(tableString);
+// }
+
 function makeTableRowsLinks(matches) {
-    //const link = "#/matches?matchId="
     const link = "#/match?matchId=";
     const matchAmount = matches.length;
     for (let i = 0; i < matchAmount; i++) {
