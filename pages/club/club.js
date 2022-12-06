2f69e02a3f97707ec62bc3f64eff6c9e1e48395c
OliverAndreasen <oliv2058@stud.kea.dk>
2022-12-06 12:11:56 +0100

    club refactor

--- pages/club/club.js
+++ pages/club/club.js
@@ -1,56 +1,58 @@
-import { clubUrl, refereeUrl, teamsUrl } from "../../settings.js";
+import {clubUrl, refereeUrl, teamsUrl} from "../../settings.js";
+import {handleHttpErrors} from "../../utils.js";
 
 const teamLink = "/#/team?teamId=";
-import { handleHttpErrors } from "../../utils.js";
+
 export function initClub() {
     window.addEventListener("load", getClub());
 }
 
-function getClub() {
+async function getClub() {
     const clubName = clubUrl + getClubFromUrl();
-    fetch(clubName)
+    await fetch(clubName)
         .then(handleHttpErrors)
-        .then((data) => {
-            console.log(data);
-            document.querySelector(".club-img").src = "./images/logos/" + data.imageString + ".png";
-            document.querySelector("#input-club-name").innerHTML = DOMPurify.sanitize(data.name);
-            document.querySelector("#input-club-address").innerHTML = DOMPurify.sanitize(data.address);
-            document.querySelector("#input-club-email").innerHTML = DOMPurify.sanitize(data.email);
-            document.querySelector("#club-name-header").innerHTML = DOMPurify.sanitize(data.name);
-            const teamHeader = DOMPurify.sanitize(data.name) + "s hold";
-            document.querySelector("#team-name-header").innerHTML = teamHeader;
-
-            createTeams(data.teams);
-            createReferees(data.referees);
+        .then((club) => {
+            createClubData(club);
+            createTeams(club.teams);
+            createReferees(club.referees);
         });
+}
+
+function createClubData(club){
+    document.querySelector(".club-img").src = "./images/logos/" + club.imageString + ".png";
+    document.querySelector("#input-club-name").innerHTML = DOMPurify.sanitize(club.name);
+    document.querySelector("#input-club-address").innerHTML = DOMPurify.sanitize(club.address);
+    document.querySelector("#input-club-email").innerHTML = DOMPurify.sanitize(club.email);
+    document.querySelector("#club-name-header").innerHTML = DOMPurify.sanitize(club.name);
+    document.querySelector("#team-name-header").innerHTML = DOMPurify.sanitize(club.name) + " hold";
 }
 
 function getClubFromUrl() {
     const splitUrl = window.location.href.split("=");
-    const clubName = splitUrl[1];
-    return clubName;
+    return splitUrl[1];
 }
 
-function createTeams(teams) {
+async function createTeams(teams) {
     if (document.querySelector(".teamsCreated") == null) {
         for (let count = 0; count < teams.length; count++) {
-            createTeam(teams[count], teams.length, count);
+            await createTeam(teams[count], teams.length, count);
         }
     }
 }
 
 async function createTeam(team, size, count) {
-    var constTeamURL = teamsUrl + team;
+    let constTeamURL = teamsUrl + team;
     const teamInfo = await fetch(constTeamURL).then(handleHttpErrors);
+    let getTeamLink = teamLink + team;
+    createTeamInfo(teamInfo, getTeamLink, size, count);
+}
 
-    var getTeamLink = teamLink + team;
-
+function createTeamInfo(teamInfo, teamLink, size, count) {
     const row = document.createElement("div");
     row.className = "row teamsCreated";
-
     row.style.cursor = "pointer";
     row.onclick = function () {
-        location.replace(getTeamLink);
+        location.replace(teamLink);
     };
 
     const nameDiv = document.createElement("div");
@@ -92,16 +94,17 @@ async function createReferees(referees) {
 
 async function createReferee(refereeName, size, count) {
     const refereeUrlGet = refereeUrl + refereeName;
-
     const referee = await fetch(refereeUrlGet).then(handleHttpErrors);
+    createRefereeInfo(referee, size, count)
+}
 
-    var refereeManager = false;
+function createRefereeInfo(referee, size, count) {
+    let refereeManager = false;
     for (let i = 0; i < referee.roles.length; i++) {
         if (referee.roles[i] === "REFEREEMANAGER") {
             refereeManager = true;
         }
     }
-
     const row = document.createElement("div");
     row.className = "row refereesCreated";
 
@@ -110,7 +113,7 @@ async function createReferee(refereeName, size, count) {
 
     const refereeNameP = document.createElement("p");
     refereeNameP.className = "mb-0";
-    if (refereeManager == true) {
+    if (refereeManager === true) {
         refereeNameP.innerText = "Dommer Ansvarlig";
     } else {
         refereeNameP.innerText = "Dommer";
@@ -138,6 +141,5 @@ async function createReferee(refereeName, size, count) {
     if (count < size - 1) {
         row.appendChild(hr);
     }
-
     document.querySelector("#referees").appendChild(row);
 }
