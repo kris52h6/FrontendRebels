8371ad88912f68d631a48d887441fc03b9d7cf29
alexander6273 <alex6273@stud.kea.dk>
2022-12-06 13:04:41 +0100

    refactor matches

--- pages/allMatches/allMatches.js
+++ pages/allMatches/allMatches.js
@@ -1,6 +1,5 @@
-import { matchesUrl, teamsUrl, clubUrl } from "../../settings.js";
+import { matchesUrl } from "../../settings.js";
 import { handleHttpErrors, sanitizeStringWithTableRows } from "../../utils.js";
-let teamsKeyValue = new Map();
 let matches;
 
 export function initAllMatches() {
@@ -9,43 +8,13 @@ export function initAllMatches() {
 
 async function setup() {
     matches = await getAllMatches();
-    const teams = await getAllTeams();
-    displayMatches(matches, teams);
-    const clubs = await fetch(clubUrl).then(handleHttpErrors);
+    displayMatches(matches);
     filterButtons();
-}
-
-function filterButtons() {
-    const buttons = document.querySelector(".btns");
-    buttons.addEventListener("mouseup", (e) => {
-        const divisionId = e.target.id;
-        filterMatches(divisionId);
-    });
-}
-
-function filterMatches(divisionId) {
-    if (divisionId === "alle") {
-        displayMatches(matches);
-        return;
-    }
-    const filteredMatches = matches.filter((m) => m.divisionName == divisionId);
-    displayMatches(filteredMatches);
 }
 
 async function getAllMatches() {
     const matches = await fetch(matchesUrl).then(handleHttpErrors);
     return matches;
-}
-
-async function getAllTeams() {
-    const teams = await fetch(teamsUrl).then(handleHttpErrors);
-    createKeyValuePairs(teams);
-    return teams;
-}
-function createKeyValuePairs(teams) {
-    for (let i = 0; i < teams.length; i++) {
-        teamsKeyValue.set(teams[i].id, teams[i].name);
-    }
 }
 
 function displayMatches(matchesData) {
@@ -63,13 +32,6 @@ function displayMatches(matchesData) {
     }
 }
 
-function matchButtons(matchId) {
-    const matchSplit = matchId.split("id");
-    matchId = matchSplit[1];
-    const link = "#/match?matchId=" + matchId;
-    location.href = link;
-}
-
 function displayMatch(m) {
     const date = new Date(m.startTime);
     const dateOptions = {
@@ -83,12 +45,12 @@ function displayMatch(m) {
     match.classList.add("match");
 
     clone.querySelector(".match-time").textContent = dateFormatted;
-    clone.querySelector(".hometeam-h2").textContent = teamsKeyValue.get(m.homeTeamId);
+    clone.querySelector(".hometeam-h2").textContent = m.homeTeamName;
     clone.querySelector(".hometeam-img").src = "./images/logos/" + m.homeTeamImg + ".png";
-    clone.querySelector(".awayteam-h2").textContent = teamsKeyValue.get(m.awayTeamId);
+    clone.querySelector(".awayteam-h2").textContent = m.awayTeamName;
     clone.querySelector(".awayteam-img").src = "./images/logos/" + m.awayTeamImg + ".png";
     clone.querySelector(".refereeteam-img").src = "./images/logos/" + m.refereeTeamImg + ".png";
-    clone.querySelector(".referee-team").textContent = teamsKeyValue.get(m.refereeTeamId);
+    clone.querySelector(".referee-team").textContent = m.refereeTeamName;
 
     match.id = "match-id" + m.id;
 
@@ -105,4 +67,28 @@ function displayMatch(m) {
     }
     match.appendChild(clone);
     matchContent.append(match);
+}
+
+function matchButtons(matchId) {
+    const matchSplit = matchId.split("id");
+    matchId = matchSplit[1];
+    const link = "#/match?matchId=" + matchId;
+    location.href = link;
+}
+
+function filterButtons() {
+    const buttons = document.querySelector(".btns");
+    buttons.addEventListener("mouseup", (e) => {
+        const divisionId = e.target.id;
+        filterMatches(divisionId);
+    });
+}
+
+function filterMatches(divisionId) {
+    if (divisionId === "alle") {
+        displayMatches(matches);
+        return;
+    }
+    const filteredMatches = matches.filter((m) => m.divisionName == divisionId);
+    displayMatches(filteredMatches);
 }
