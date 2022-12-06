2e17647e3393a50199fcb838766370cf9ac09617
OliverAndreasen <oliv2058@stud.kea.dk>
2022-12-06 10:27:59 +0100

    inputfields

--- pages/createReferee/createReferee.js
+++ pages/createReferee/createReferee.js
@@ -1,39 +1,30 @@
-
-import{refereeUrl} from "../../settings.js";
+import {refereeUrl} from "../../settings.js";
 import {handleHttpErrors, validateAllObjectWhiteSpaces, checkIfEmptyObject} from "../../utils.js";
 
 
-export function initCreateReferee(){
+export function initCreateReferee() {
 
     window.addEventListener("load", createReferee())
 }
 
-async function createReferee(){
+async function createReferee() {
     document.querySelector("#btn-user-add").onclick = makeNewReferee;
 
-    async function makeNewReferee(){
-        const newReferee = {}
-        newReferee.username = document.querySelector("#input-user-username").value
-        newReferee.email = document.querySelector("#input-user-email").value
-        newReferee.password = document.querySelector("#input-user-password").value
-        newReferee.firstname = document.querySelector("#input-user-firstname").value
-        newReferee.lastname = document.querySelector("#input-user-lastname").value
-        newReferee.bankInformation = document.querySelector("#input-user-bankinformation").value
-        newReferee.license = document.querySelector("#input-user-license").value
+    async function makeNewReferee() {
 
+        const newReferee = inputFields()
 
-        if(checkIfEmptyObject(newReferee)){
+        if (checkIfEmptyObject(newReferee)) {
             console.log("mangler felter")
             const errorDiv = document.querySelector("#error")
             errorDiv.innerHTML = "Venligst udfyld alle felter"
             errorDiv.removeAttribute("hidden")
-        }else if (validateAllObjectWhiteSpaces(newReferee)) {
+        } else if (validateAllObjectWhiteSpaces(newReferee)) {
             console.log("har mellemrum")
             const errorDiv = document.querySelector("#error")
             errorDiv.innerHTML = "Du må ikke bruge mellemrum"
             errorDiv.removeAttribute("hidden")
-        }
-        else {
+        } else {
             const options = {}
             options.method = "POST"
             options.headers = {"Content-type": "application/json"}
@@ -48,5 +39,19 @@ async function createReferee(){
                 errorDiv.removeAttribute("hidden")
             })
         }
-        }
     }
+}
+
+function inputFields() {
+    const newReferee = {}
+
+    newReferee.username = document.querySelector("#input-user-username").value
+    newReferee.email = document.querySelector("#input-user-email").value
+    newReferee.password = document.querySelector("#input-user-password").value
+    newReferee.firstname = document.querySelector("#input-user-firstname").value
+    newReferee.lastname = document.querySelector("#input-user-lastname").value
+    newReferee.bankInformation = document.querySelector("#input-user-bankinformation").value
+    newReferee.license = document.querySelector("#input-user-license").value
+
+    return newReferee
+}
