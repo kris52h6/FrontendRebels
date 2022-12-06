467c75f5af25b5d47e8aca087d84da6e49b97fa9
alexander6273 <alex6273@stud.kea.dk>
2022-12-06 13:41:40 +0100

    refactor teams

--- pages/mySignups/mySignups.js
+++ pages/mySignups/mySignups.js
@@ -1,20 +1,16 @@
-import {refereeUrl, teamsUrl, clubUrl,myMatchesAccepted } from "../../settings.js";
+import {refereeUrl, myMatchesAccepted } from "../../settings.js";
 import { handleHttpErrors, sanitizeStringWithTableRows } from "../../utils.js";
 
 export function initMySignups() {
     setup();
 }
 
-let teamsKeyValue = new Map();
 let matches;
 
 async function setup() {
     matches = await getAllMatches();
-    console.log(matches)
-    displayMatches(matches, teams);
-    const clubs = await fetch(clubUrl).then(handleHttpErrors);
+    displayMatches(matches);
 }
-
 
 async function getAllMatches() {
     const username = await getUserName()
@@ -30,17 +26,6 @@ async function getUserName(){
     options.headers = {"Authorization": token}
     const response = await fetch(refereeUrl,options).then(handleHttpErrors)
     return response.username
-}
-
-async function getAllTeams() {
-    const teams = await fetch(teamsUrl).then(handleHttpErrors);
-    createKeyValuePairs(teams);
-    return teams;
-}
-function createKeyValuePairs(teams) {
-    for (let i = 0; i < teams.length; i++) {
-        teamsKeyValue.set(teams[i].id, teams[i].name);
-    }
 }
 
 function displayMatches(matchesData) {
@@ -78,12 +63,12 @@ function displayMatch(m) {
     match.classList.add("match");
 
     clone.querySelector(".match-time").textContent = dateFormatted;
-    clone.querySelector(".hometeam-h2").textContent = teamsKeyValue.get(m.homeTeamId);
+    clone.querySelector(".hometeam-h2").textContent = m.homeTeamName;
     clone.querySelector(".hometeam-img").src = "./images/logos/" + m.homeTeamImg + ".png";
-    clone.querySelector(".awayteam-h2").textContent = teamsKeyValue.get(m.awayTeamId);
+    clone.querySelector(".awayteam-h2").textContent = m.awayTeamName;
     clone.querySelector(".awayteam-img").src = "./images/logos/" + m.awayTeamImg + ".png";
     clone.querySelector(".refereeteam-img").src = "./images/logos/" + m.refereeTeamImg + ".png";
-    clone.querySelector(".referee-team").textContent = teamsKeyValue.get(m.refereeTeamId);
+    clone.querySelector(".referee-team").textContent = m.refereeTeamName;
 
     match.id = "match-id" + m.id;
 
